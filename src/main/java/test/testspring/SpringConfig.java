b27cfc61697e0057c2019828bc665bed3acca944
kang-dae-hyun <kdh487@gmail.com>
2021-06-30 00:25:30 +0900

    diificult

--- src/main/java/test/testspring/SpringConfig.java
+++ src/main/java/test/testspring/SpringConfig.java
@@ -1,13 +1,25 @@
 package test.testspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import test.testspring.repository.JdbcMemberRepository;
+import test.testspring.repository.JdbcTemplateMemberRepository;
 import test.testspring.repository.MemberRepository;
 import test.testspring.repository.MemoryMemberRepository;
 import test.testspring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+        private DataSource dataSource;
+
+        @Autowired
+        public SpringConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+    }
 
         @Bean
         public MemberService memberService() {
@@ -16,6 +28,8 @@ public class SpringConfig {
 
         @Bean
         public MemberRepository memberRepository() {
-            return new MemoryMemberRepository();
+        //    return new JdbcMemberRepository(dataSource);
+        //    return new MemoryMemberRepository();
+            return new JdbcTemplateMemberRepository(dataSource);
         }
 }
---
+++ src/test/java/test/testspring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+package test.testspring.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import test.testspring.domain.Member;
+import test.testspring.repository.MemberRepository;
+//import test.testspring.repository.MemoryMemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("명재환");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+
+    }
+
+    @Test
+    public void 중복확인() throws Exception{
+        // given
+        Member member1 = new Member();
+        member1.setName("유영석");
+
+        //when
+        Member member2 = new Member();
+        member2.setName("유영석");
+
+        //then
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다."); }
+
+
+}
